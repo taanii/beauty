c0c19fcd85b333d626519236aa1a8238962f7386
chaitanyakeerthi <kchatsemo@gmail.com>
2020-12-04 19:55:46 +0530

    backend

--- connection.php
+++ connection.php
@@ -5,14 +5,15 @@ $password = "";
 $dbname = "form";
 $name = $_POST['Name'];
 $email = $_POST['Email'];
+$mobile = $_POST['Mobile'];
 $message = $_POST['Message'];
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 die("Connection failed: ". $conn->connect_error);
 }
 
-$sql = "INSERT INTO details (Name, Email, Message)
-VALUES ('$name', '$email', '$message')";
+$sql = "INSERT INTO details (Name, Email, Mobile, Message)
+VALUES ('$name', '$email', '$mobile', '$message')";
 
 if($conn->query($sql) === TRUE) {
 echo "New record created successfully";
